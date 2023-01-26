fb10e1040a08b7cced1f9e15426e7b2e7fd5aded
Stuart Davies <stuart.davies@junitech.com>
2023-01-26 22:08:49 +0000

    Added more content

--- src/Pages/OurSupporters.js
+++ src/Pages/OurSupporters.js
@@ -6,6 +6,56 @@ function OurSupporters() {
             <NavBar />
             
             <h1>Our supporters</h1>
+
+            <h2>Nimbus and The Access Card</h2>
+
+            <img src="/images/nimbus-disability-logo.jpg" alt="Nimbus Disability logo"></img>
+            <p>Nimbus Disability is a social Enterprise set up to support businesses to meet and exceed their legal obligations toward disabled people. An outcome of our 15+ years of experience became a scheme called The Access Card.</p>
+            
+            <p><strong>The Access Card</strong> is a photo ID card for disabled people that communicates their access requirements (and legal rights) through a series of symbols on the card. The idea being that it is quick, easy and discreet for disabled people to ask for, and service providers to offer, critical reasonable adjustments.</p>
+            <img src="/images/access-card.jpg" width={300} alt="The Access Card"></img>
+
+            <p>Visit the <a href="https://www.accesscard.online/find-a-credable-provider/map-of-providers/">website</a> to check out 1,000 venues and visitor attractions across the UK and beyond that recognise the Card</p>
+
+            <h2>The Limbless Association</h2>
+
+            <p>The Limbless Association (LA) is a national charity that's been supporting the limb loss community for 37 years. The LA supports amputees pre and post-amputation with a range of services, resources and connectivity. These include the LA’s Help Desk (welfare rights, rehabilitation pathways, access to legal support and multi-agency signposting), LA Volunteer Visitor (peer support), Support and Connect Hubs (national), strategic representation on a number of national bodies, LA quarterly magazine StepForward and much more. LA membership is free to all amputees across the UK. Contact by phone 0800 644 0185 or <a href="mailto:enquires@limbless-association.org">enquires@limbless-association.org</a></p>
+
+            <p>Unit 10, Waterhouse Business Centre, 2 Cromar Way, Chelmsford, Essex, CM1 2QE</p>
+            <p>01245 216670</p>
+            <p>www.limbless-association.org</p>
+            <p>http://www.facebook.com/limblessassociationhttp://www.twitter.com/limblessassoc</p>
+
+            <img src="/images/limbless-association-banner.jpg" alt="Limbless Association"></img>
+
+            <h2>The Bone Cancer Research Trust</h2>
+            <p>The Bone Cancer Research Trust is the leading charity dedicated to ﬁghting primary bone cancer and bone tumours. Our mission is to save lives and improve outcomes for people affected by primary bone cancer through research, information, awareness and support.</p>
+            <p>The Bone Cancer Research Trust funds pioneering research into all forms of primary bone cancer and ensures that accurate information and support are available for patients of all ages whilst raising national and international awareness of this devastating disease. To find out more about the charity please visit bcrt.org.uk or for any help or support contact our Support & Information Service on 0800 111 4855/support@bcrt.org.uk</p>
+
+            <img src="/images/bone-cancer-research-trust-banner.jpg" width={500} alt="Bone Cancer Research Trust"></img>
+
+            <h2>Macmillan Cancer Support</h2>
+
+            <p>Macmillan offers comprehensive expert information and practical support on a wide range of matters.  From clinical and well-being information to legal and financial support and advice.  In some areas Macmillan volunteers offer practical support to patients on their return from hospital.</p>
+            <p>Find out more about how Macmillan can help YOU at https://www.macmillan.org.uk/cancer-information-and-support</p>
+
+            <img src="/images/macmillan-cancer-support-logo.png" width={400} alt="Macmillan Cancer Support"></img>
+
+            <h2>Sarcoma UK</h2>
+
+            <p>Sarcoma UK is a national charity that funds vital research, offers support for anyone affected by sarcoma and campaigns for better treatments. It is the only cancer charity in the UK focusing on all types of sarcoma.</p>
+            <p>https://sarcoma.org.uk/about-us</p>
+ 
+ 
+            <p>Contact details:
+ 
+            Sarcoma UK Support Line:
+            Phone: 0808 801 401
+            Email: supportline@sarcoma.org.uk
+            Text: 07860058830 
+            </p>
+ 
+            <img src="/images/sarcoma-uk-logo.jpg" width={300} alt="Sarcoma UK"></img>
         </>
     );
 }
--- src/Pages/Prosthetics.js
+++ src/Pages/Prosthetics.js
@@ -4,8 +4,124 @@ function Prosthetics() {
     return (
         <>
             <NavBar />
-            
-            <h1>Prosthetics</h1>
+
+            <h1>The Prosthetic Care of Hip Disarticulation and Hemi-pelvectomy Amputees</h1>
+
+            <h2>Socket and Prescription Choices</h2>
+
+            <p>Author: Jane Mclean, Senior Prosthetist, Blatchford (30/09/2020)</p>
+
+            <p>Hip disarticulation and hemi-pelvectomy are rare amputation sites, accounting for approximately 0.5% of lower limb amputations in UK. Causes of amputations at these levels include cancer, infection, intravenous drug use and trauma.</p>
+            <p>Not all amputees at this level are suitable for prosthetic rehabilitation but they are usually given the opportunity to discuss their options with the multi-disciplinary team.</p>
+            <p>We are constantly seeking new and innovative ways to improve the outcomes of these particularly complex patients.</p>
+
+            <h3>Sockets</h3>
+
+            <p>Until 2016, our Hip disarticulation/ hemipelvectomy sockets had been made primarily with flat seat for ischial/soft tissue bearing and suspension over iliac crests.</p>
+
+            Figure 1
+
+            <p>We cast using a casting table, angled blocks for hip angles and ropes/chains for pulling in over iliac crests. This creates a 3-point force system (coronal plane) which stabilises the pelvis in the socket. Anterior-posterior control is achieved with good volume matching (hydro-static control) and a snug A-P dimension in the socket.</p>
+
+            <p>The high trimlines can restrict mobility of the trunk, and pressure above iliac crests can be uncomfortable. This often results in users loosening their sockets to relieve this discomfort, which leads to a reduction in socket fit, and so a reduction in pelvic control.</p>
+
+            <p>Ambulation using a hip disarticulation prosthesis requires 100-200% more energy expenditure than “normal” gait. Having lost approximately 18% of their total body surface area due to their amputation, the body’s thermoregulation has been compromised. The high trimlines of the traditional hip socket further compounds this issue as it covers an additional approximately 19%, further reducing the body’s ability to control temperature. This results in the user suffering from perspiration in the socket and the issues this brings, and can be very uncomfortable.</p>
+
+            <p>In 2016, we became aware of the Bikini style socket and began evaluating its use in practice, as a result we now routinely incorporate this into our practice.</p>
+
+            <h3>What is the bikini socket?</h3>
+
+            <p>As the name suggests, the socket covers as little as possible while still serving its purpose.</p>
+
+            <p>The socket has anterior and posterior “wings” (a) which stabilise the pelvis front to back, and flexible iliac crest stabilisers (b) on either side to provide medio-lateral control and suspension of the prosthesis.</p>
+
+            Figure 2.
+
+            <p>The lateral trim on the amputated side (c) is cut low for comfort - the strap now serving the purpose that until now the high lateral trim had been doing.</p>
+
+            <p>The rigid "belt" on the contralateral side which secure the amputee in the socket and suspend over the contralateral iliac crest has also been replaced with a flexible strap. This allows the rigid socket to end a little past the body’s midline.</p>
+
+            <p>The anterio-posterior dimension of the socket is set by the prosthetist, and does not change if the user tightens or loosens the socket. This maintains an intimate skeletal fit, reduced movement of the pelvis within the socket, and so greater control and responsiveness of the prosthesis.</p>
+
+            <p>Flexible padded iliac crest stabilisers will conform to the changing shape of the users' body during different gait and when seated, increasing comfort in the socket.</p>
+
+            <p>The reduced trimlines mean that the bikini is much less restrictive, allowing greater trunk mobility without compromising suspension or comfort.</p>
+
+            <p>Complications such as stoma bags or nephrostomy sites can be avoided using careful trimline management.</p>
+
+            <p>The reduced trimlines allow release of perspiration, improving the user's ability to regulate temperate, and provides a very lightweight socket.</p>
+
+            <h3>Outcomes</h3>
+
+            <p>To date we have fitted eleven bikini sockets to five hip disarticulation and six hemi-pelvectomy amputees.</p>
+
+            <p>Five of these were established amputees, six were primaries.</p>
+
+            <p>Nine patients continue to successfully wear the bikini socket.</p>
+
+            <p>Two of the established amputees rejected the bikini socket, reporting that they felt more secure in their traditional type socket. (They are both hip disarticulation amputees)</p>
+
+            <h3>Components</h3>
+
+            <p>The standard limb built for these amputation levels consists of a single axis hip joint, a four bar knee and a SACH (solid ankle, cushion heel) type foot.</p>
+
+            <p>The hip joint may or may not incorporate a stride limiter depending on the ability of the user.</p>
+
+            <p>The four bar knee is used as it is geometrically stable and hence very safe for high level amputees.</p>
+
+            <p>The SACH type foot allows rapid progression to foot flat and so increases the period of double support (the length of time that the user has both feet on the ground).</p>
+
+            <p>Since NHS England changed the policy on microprocessor knees (MPK), some of our patients have met the inclusion criteria and been successfully fitted.</p>
+
+            <p>Microprocessor controlled knees, adapt hydraulic resistance in real time, providing the wearer with support when moving in any environment or standing still. They also have stumble recovery technology, ensuring that the knee remains stable should the user falter. This reduces this risk of amputees falling or injuring themselves when walking or changing environments, and provides users with the confidence and stability needed to move without fear. Such benefits not only make a huge difference to patient safety and quality of life; they also reduce the lifelong care needs of amputees.</p>
+
+            <p>Prevention of falls is just one aspect - reducing the chances of acute injuries - but effective stabilisation can also cut the knock-on medical problems often experienced by amputees with conventional prosthesis and joints, such as lower-back pain, arthritis and joint replacements.</p>
+
+            <p>The prescription of a foot with a hydraulic ankle further improves gait, again, these feet allow for a longer period of double support while also allowing a more natural and symmetrical gait pattern.</p>
+
+            <h3>Case Study</h3>
+
+            <p>This 34 year old female was first seen by the multidisciplinary team (consultant, physiotherapist, and prosthetist) in October 2014.</p>
+
+            <p>She had a right hip disarticulation amputation and a right ileostomy in May 2014 due to an infection.</p>
+
+            <p>She had extensive skin grafts over her amputation site and unhealed donor sites on left thigh.</p>
+
+            <p>We discussed future prosthetic use, showed her a prosthesis and explained the rehabilitation process (casting, fitting, physiotherapy).</p>
+
+            <p>At this point the decision was made to defer prosthetic rehabilitation until after reversal of ileostomy. This would also allow time for the skin grafts and donor sites to heal fully.</p>
+
+            <p>In the meantime she ambulated using a wheelchair when out and with crutches indoors. She returned to work in November.</p>
+
+            <p>We started work on her primary limb in March 2015. This consisted of a traditional style laminated socket with a soft northvane liner, single axis hip joint, 4 bar knee and SACH type foot.</p>
+
+            <p>Rehabilitation progressed well with physiotherapy and at the three month review stage she reported that she was wearing the prosthesis for up to five hours a day, having no skin issues and was pleased with her progress. She reported a SCS (socket comfort score) of 7.</p>
+
+            <p>In 2016 we made a bikini socket and she was delighted with the improvement in comfort. She reported an improved SCS of 8-9.</p>
+
+            <p>After three months use her feedback was:</p>
+
+            <ul>
+                <li>Her prosthesis is lighter</li>
+                <li>Improved comfort around her waist</li>
+                <li>Improved comfort in sitting- much easier to drive</li>
+                <li>Easier to don and doff</li>
+                <li>More cosmetic- can wear tighter clothes</li>
+                <li>However- she was still only limb wearing at work and when she was out, approximately 5 hours/day due to fear of falling</li>
+            </ul>
+
+            <p>We put her forward for MPK pre- assessment in April 2017 and she was accepted onto a four week trial, below are her outcome measures using initially the original prescription then after 4 weeks trial on MPK/hydraulic ankle.( Her hip joint remained unchanged)</p>
+
+            Table
+
+            <p>The notable improvements are the elimination of trips and stumbles and the increased distance walked on her 2 and 6 minute walking trails (61% and 57% respectively). There was also an excellent improvement in her TUG (timed up and go) and the 10m timed walk.</p>
+
+            <p>She also reported feeling more confident on her prosthesis and was using it for up to 8 hours a day. She was using one walking stick outdoors and could manage without it indoors.</p>
+
+            <p>She continues to use this prosthesis successfully.</p>
+
+            <h3>Conclusion</h3>
+            <p>Patients with hip disarticulation and hemi-pelvectomy levels of amputation are challenging to rehabilitate, both for the patients and the clinicians. However, excellent outcomes can be attained with careful consideration of socket styles and components and good physiotherapy input.</p>
         </>
     );
 }
