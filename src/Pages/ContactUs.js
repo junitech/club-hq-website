11deef2f451c46e356f441e62017f87fd9ec9894
Stuart Davies <stuart.davies@junitech.com>
2023-01-26 01:10:39 +0000

    Added contact form

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -6,6 +6,12 @@ function ContactUs() {
             <NavBar />
             
             <h1>Contact us</h1>
+
+            <form action="https://formspree.io/f/xayzqyna" method="POST">
+                <input type="email" name="_replyto" />
+                <textarea name="body"></textarea>
+                <input type="submit" value="Send" />
+            </form>            
         </>
     );
 }
