f10caedcf48f3ca56374eb4e60071e83d3caec32
Claire Davies <claire.davies@junitech.com>
2023-01-30 22:09:45 +0000

    added bootstrap and modified navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,37 +1,71 @@
-import './NavBar.css';
+//import './NavBar.css';
 
 function NavBar() {
     return (
         <>
-            <nav className="navbar">
-                <ul>
-                    <li><a href="/">Home</a></li>
-
-                    <li><a href="/about-club-hq">About Club HQ</a></li>
-
-                    <li><a href="/about-the-surgery">About the surgery</a></li>
-                    <li><a href="/prosthetics">Prosthetics</a></li>
-                    <li><a href="/mental-health">Mental health</a></li>
-                    <li><a href="/phantom-pain">Phantom pain</a></li>
-
-                    <li><a href="/mentoring-and-peer-support">Mentoring and peer support</a></li>
-                    <li><a href="/facebook-secret-group">Facebook secret group</a></li>
-                    <li><a href="/fitness">Fitness</a></li>
-
-                    <li><a href="/financial-support">Financial support</a></li>
-                    <li><a href="/travel-and-entertainment">Travel and entertainment</a></li>
-                    <li><a href="/group-video-calls">Group video calls</a></li>
-                    <li><a href="/wheelchairs">Wheelchairs</a></li>
-
-                    <li><a href="/sam-coleman">Sam Coleman</a></li>
-                    <li><a href="/martin-austin">Martin Austin</a></li>
-                    <li><a href="/members">Members</a></li>
-                    <li><a href="/one-mans-journey">One man's journey</a></li>
-
-                    <li><a href="/faqs">FAQs</a></li>
-                    <li><a href="/our-supporters">Our supporters</a></li>
-                    <li><a href="/contact-us">Contact us</a></li>
-                </ul>
+            <nav className="navbar navbar-expand-sm bg-primary ">
+                <div className="container-fluid">
+                <a className="navbar-brand" href="/">Club HQ</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                        
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">Home</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="/">Home</a></li>
+                                    <li><a className="dropdown-item" href="/about-club-hq">About Club HQ</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">About Hindquarter Amputations</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="/about-the-surgery">About the surgery</a></li>
+                                    <li><a className="dropdown-item" href="/prosthetics">Prosthetics</a></li>
+                                    <li><a className="dropdown-item" href="/mental-health">Mental health</a></li>
+                                    <li><a className="dropdown-item" href="/phantom-pain">Phantom pain</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">Living with an HQA</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="/mentoring-and-peer-support">Mentoring and peer support</a></li>
+                                    <li><a className="dropdown-item" href="/facebook-secret-group">Facebook secret group</a></li>
+                                    <li><a className="dropdown-item" href="/fitness">Fitness</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">Practical Support</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="/financial-support">Financial support</a></li>
+                                    <li><a className="dropdown-item" href="/travel-and-entertainment">Travel and entertainment</a></li>
+                                    <li><a className="dropdown-item" href="/group-video-calls">Group video calls</a></li>
+                                    <li><a className="dropdown-item" href="/wheelchairs">Wheelchairs</a></li>
+                                </ul>
+                            </li>
+                             <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">Stories and Bios</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="/sam-coleman">Sam Coleman</a></li>
+                                    <li><a className="dropdown-item" href="/martin-austin">Martin Austin</a></li>
+                                    <li><a className="dropdown-item" href="/members">Members</a></li>
+                                    <li><a className="dropdown-item" href="/one-mans-journey">One man's journey</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/faqs">FAQs</a>
+                            </li>
+                            <li class="nav-item">
+                                <a className="nav-link" href="/our-supporters">Our supporters</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/contact-us">Contact us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </>
     );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
