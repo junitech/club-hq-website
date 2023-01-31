9de0c2f431318f10eb70829b5ec525eeef7bd7f9
Stuart Davies <stuart.davies@junitech.com>
2023-01-31 16:16:46 +0000

    Added pain and travel content

--- src/Pages/PhantomPain.js
+++ src/Pages/PhantomPain.js
@@ -3,6 +3,58 @@ function PhantomPain() {
     return (
         <>
             <h1>Phantom pain</h1>
+
+            <p>Author: Dr Dominic Aldington FRCA FFPMRCA</p>
+            <p>has specialised exclusively in pain management for the last 10 years and lectured on the subject around the world. He is the Clinical Lead and Consultant in Pain Medicine at Winchester Hospital and Hampshire Hospitals NHS Trust. Having previously served in the Royal Army Medical Corps for 20 years, his many appointments include Chairman of the UK Armed Forces Clinical Forum, Vice-chairman of the IASP Special Interest Group on the Pain of Torture, Organised Violence and War, and the Surgeon General's Advisor of Veterans' Health and is involved in the development of pain clinics for military veterans.</p>
+
+            <p>I've been invited to write a few words on the subject of pain for your group. However, before I do I would like to start by saying what a great project I think this is and how honored I am to have been invited to participate.</p>
+
+            <p>I won't bore you with my credentials as you are quite capable of hunting me down on the Internet.</p>
+
+            <p>Although I have been tasked with focusing on a few seemingly simple issues, I think that the topic of pain, particularly following amputation, is a most difficult thing and so I would like to try to start near the beginning, but rather than diving into the weeds will try to provide the headlines.</p>
+
+            <p>Before talking further, I think we should all agree on what pain is. The new definition of the pain, from the International Association for the Study of Pain, is:</p>
+
+            <p>"An unpleasant sensory and emotional experience associated with, or resembling that associated with, actual or potential tissue damage,"</p>
+
+            <p>There are 6 key points added to this:</p>
+
+            <ol>
+                <li>Pain is always a personal experience that is influenced to varying degrees by biological, psychological, and social factors.</li>
+                <li>Pain and nociception (the electrical message in the nerves) are different phenomena. Pain cannot be inferred solely from activity in sensory neurons.</li>
+                <li>Through their life experiences, individuals learn the concept of pain.</li>
+                <li>A person's report of an experience as pain should be respected.</li>
+                <li>Although pain usually serves an adaptive role, it may have adverse effects on function and social and psychological well-being.</li>
+                <li>Verbal description is only one of several behaviours to express pain; inability to communicate does not negate the possibility that a human or a nonhuman animal experiences pain.</li>
+            </ol>
+
+            <p>This highlights the importance of both the emotional and sensory components of pain, the personal nature of pain and that further damage does not have to be present for the pain to be experienced. Indeed, in the absence of damage, pain is still pain.</p>
+
+            <p>One of the many problems with pain is that we are led to believe that it always indicates damage and that the damage and the pain perceived are in some way proportional to one another. Thus, if you have big pain you must have big damage and vice a versa. Unfortunately, this is a fallacy and you can have dreadful pain and little damage, and you can have dreadful damage and little pain. In either way, if we are treating pain, then pain is real whenever anyone says it is.</p>
+
+            <p>The other point is that we, healthcare workers, cannot tell you how bad your pain is any more than you can tell us how bad our pain is. Pain is a personal thing and will vary. The good news is I do not need to tell you how bad your pain is, as you can tell me.</p>
+
+            <p>The mechanisms for phantom sensation and phantom pain are not agreed and there are a number of theories. When discussing pains associated with amputation healthcare workers often get very excited and erroneously ascribe everything to phantom pain. Phantom pain describes the pain in the part of the body that no longer exists. Phantom sensation describes a non-painful sensation in the part of the body that no longer exists. A stump pain is a pain in the "residual limb" if there is any.</p>
+
+            <p>Similarly, the best treatment modalities are also unclear, and it seems likely that an individual needs to work through quite a list of things until they find something that works for them. No intervention is likely to remove all the pain and in fact the aim should be providing a treatment that stops the pain being a problem. Research would suggest people can cope with a mild pain but when the pain gets more severe than mild, coping mechanisms fail.</p>
+
+            <p>So now we understand that pain is a fairly complex thing we need to discuss the options for its management.</p>
+
+            <p>I would suggest the first thing to do with the pain it's understand what it means to you. If you believe it indicates something terrible going on, then you will need to have it investigated and treated invasively. Sadly, this may not change the pain and can even make it worse. If you accept the pain is there, it is real, but it does not indicate anything dreadful happening, then you are more likely to be able to engage in symptom management and master your pain.</p>
+
+            <p>It is probably fair to say that the longer one has a pain the more significant the emotional component becomes. Broadly speaking, the emotional component is made up of anxiety, unhappiness, and frustration (known by many of my patients as the “F' in pain”).</p>
+
+            <p>For the emotional components, one can look at specific psychological therapies or pain management programs, and some forms of medication may be of benefit.</p>
+
+            <p>For the physical components of pain there are a number of creams and gels, and pills and potions that can be taken but none of these are wonderful for everyone and very often for some people they cause more trouble than they're worth.</p>
+
+            <p>Another concept to manage is the ownership of the pain. Many healthcare professionals will offer to help and in doing so may subjugate your position. However, the pain is yours and you must make the decisions about what you do, since only you will get benefits and only you will suffer side-effects and it is your life that is being affected.</p>
+
+            <p>The bottom line is that some people are left with pain, but that does not mean they cannot live a full and meaningful life.</p>
+
+            <p>Editor's note: Pain management for HQAs can range from medication, creams, heat, mirror therapy, TENS machines, acupuncture, psychological therapies, EMDR, hypnotism and much more. Each person needs to find their own best pain management by trial and error. It is not an exact science.</p>
+
+            <p>It is hoped that we may in future be able to participate in Dr Aldington's online pain management courses that he currently runs for veterans.</p>
         </>
     );
 }
--- src/Pages/TravelAndEntertainment.js
+++ src/Pages/TravelAndEntertainment.js
@@ -3,6 +3,113 @@ function TravelAndEntertainment() {
     return (
         <>
             <h1>Travel and entertainment</h1>
+
+            <p>Ian Coldicott is a hindquarter amputee and co-Founder of Club HQ</p>
+
+            <h2>Driving</h2>
+
+            <h3>Driving with a medical condition, Blue Badges, public transport and using airports as a hind quarter amputee</h3>
+            <p>This link is the best place to start. Details of what's covered are in the following paragraphs. https://www.gov.uk/browse/driving/disability-health-condition</p>
+
+            <h3>Driving with a medical condition</h3>
+            <p>You must tell DVLA if you hold a driving licence and have had a limb amputated. You can be fined if you don't tell DVLA about a medical condition that affects your driving. To report your medical condition, fill in form DVLA form G1 (see below).</p>
+            <p>https://www.gov.uk/government/publications/g1-online-confidential-medical-information</p>
+            <p>You will usually get a decision within six weeks. DVLA might ask you to take a driving assessment.</p>
+
+            <img src="/images/dvla-form-g1.jpg" class="center-image" alt="" />
+
+            <p>Licence holders wishing to drive after surgery should establish with their own doctor when it would be safe to do so. Drivers must ensure they remain covered by insurance to drive after surgery. You must surrender your licence to DVLA if your doctor tells you that you need to stop driving for three months or more because of your medical condition.</p>
+
+            <h3>Disabled Driving Centres</h3>
+
+            <p>There are 20 main disabled driving centres throughout the UK: each centre has satellite driving centres which may be more conveniently located. Special conditions apply if you live in Scotland or Wales.</p>
+
+            <p>https://www.drivingmobility.org.uk/find-a-centre/</p>
+
+            <p>These are independent disabled driving organisations that provide a range of mobility assessment services and advice. They provide driving tuition and assessment using dual control adapted vehicles, including vehicles fitted with hand controls or left foot accelerators. Many centres will accept self referrals. Prices vary form centre to centre.</p>
+
+            <p>https://www.drivingmobility.org.uk/information/physical-disability/</p>
+
+            <h3>Vehicle adaptations</h3>
+
+            <p>You should be able to drive a car with automatic transmission, though if you have a left leg you would need to have a left foot accelerator fitted. This would cost around £450 including fitting from the best known supplier Jeff Gosling Hand Controls Ltd. or from a suitably experienced vehicle adaptation specialist. Alternatively, you may prefer to have the vehicle adapted with hand controls. You may also wish to have a hoist fitted to your vehicle to make a wheelchair or mobility scooter easier to load on and off.</p>
+
+            <p>https://www.jeffgosling.co.uk</p>
+
+            <h3>Vehicle tax exemption</h3>
+
+            <p>You can apply for exemption from paying vehicle tax if you get at least one of:</p>
+
+            <ul>
+                <li>higher rate mobility component of Disability Living Allowance (DLA)</li>
+                <li>enhanced rate mobility component of Personal Independence Payment (PIP)</li>
+                <li>War Pensioners' Mobility SupplementArmed Forces Independence Payment</li>
+            </ul>
+
+            <p>The vehicle must be registered in the disabled person's name or their nominated driver's name.</p>
+
+            <p>It must only be used for the disabled person's personal needs. It cannot be used by the nominated driver for their own personal use.</p>
+
+            <p>You can get a 50% reduction in vehicle tax if you get the PIP standard rate mobility component. The vehicle should be registered in the name of either the disabled person or their nominated driver.</p>
+
+            <h3>Toll road concessions</h3>
+            <p>You may not have to pay the full fare at some river crossings, bridges and tunnels if you’re disabled. In most cases, you have to apply in advance to get a concession.</p>
+            <p>https://www.gov.uk/toll-concessions</p>
+
+            <h3>Motability</h3>
+
+            <img src="/images/motability.jpg" class="center-image" alt="" />
+
+            <p>The Motability Scheme enables anyone in receipt of a higher rate mobility allowance (such as the Enhanced Rate of the Mobility Component of Personal Independence Payment or the Higher Rate Mobility Component of Disability Living Allowance) to use their mobility allowance to lease a car, scooter, powered wheelchair or Wheelchair Accessible Vehicle.</p>
+
+            <p>You exchange all, or part of your mobility allowance to lease the vehicle of your choice. You choose the vehicle you want at a price that works for you , and payments are deducted from your higher rate mobility allowance every four weeks, then paid directly to Motability by the DWP.</p>
+
+            <p>The standard lease is over three years, or five years if you are leasing a Wheelchair Accessible Vehicle and Motability take care of running costs such as insurance for up to three named drivers (this doesn’t have to be you), servicing, maintenance, breakdown cover, tyre repair and replacement and windscreen repairs.</p>
+
+            <p>https://www.motability.co.uk/about/how-the-scheme-works/</p>
+
+            <p>If you are eligible for the Scheme, you should contact a Motability dealer and arrange an appointment with their Motability Scheme specialist.</p>
+
+            <h3>VAT relief</h3>
+
+            <p>You may not have to pay VAT on having a vehicle adapted to suit your condition, or on the lease of a Motability vehicle.</p>
+
+            <h3>Mobility scooters and powered wheelchairs</h3>
+
+            <p>You do not need a licence to drive a mobility scooter or powered wheelchair, but you may have to register it. Only certain types can be driven on the road. The law calls these types of vehicles ‘invalid carriages’.</p>
+
+            <p>Mobility scooters and powered wheelchairs come in 2 categories:</p>
+
+            <ul>
+                <li>‘class 2 invalid carriages’ - these cannot be used on the road (except where there is not a pavement) and have a maximum speed of 4mph</li>
+                <li>‘class 3 invalid carriages’ - these can be used on the road, and have a maximum speed of 4mph off the road, and 8mph on the road</li>
+            </ul>
+
+            <p>You do not need to register a class 2 invalid carriage. You must register class 3 invalid carriages.</p>
+
+            <p>https://www.gov.uk/mobility-scooters-and-powered-wheelchairs-rules</p>
+
+            <h3>The Blue Badge scheme</h3>
+
+            <p>Blue Badges help disabled people park closer to their destination, whether as drivers or passengers. You qualify automatically for a badge if you meet at least one of certain benefit-related criteria: https://www.gov.uk/government/publications/blue-badge-can-i-get-one/can-i-get-a-blue-badge</p>
+
+            <img src="/images/blue-badge.png" class="center-image" alt="" />
+
+            <p>You are not, as hind quarter amputee (HQA) automatically entitled to a Blue Badge but you should encounter no difficulty in being awarded one, as you are eligible on the basis that ‘you cannot walk at all.’ Start here: https://www.gov.uk/apply-blue-badge to confirm that you may be eligible, and apply to your local authority. Follow this link to find out how to apply and what documents you’ll need to provide.</p>
+
+            <p>Hints on completing the application form for a Blue Badge:</p>
+
+            <em>A HQA is an ‘enduring and substantial disability that means you are unable to walk at al during the course of a journey. The Blue Badge scheme local authority guidance (England) (Updated 30 August 2019), para. 4.42, states that, in assessing applicants, “Being unable to walk means that they cannot take a single step. The applicant needs to show that, because of their enduring and substantial disability, they cannot walk during the course of a journey. Walking involves always having one foot on the ground. And in para. 4.43: ‘If the applicant’s only way of getting about is to swing through two elbow crutches, then they will be considered unable to walk (provided it is due to an enduring and substantial disability…’.</em>
+
+            <p>A Blue badge costs up to £10 in England and £20 in Scotland. It’s free in Wales. It normally runs for three years before needing renewal. Your council will determine your eligibility. If they decide that you are not eligible and you think that they did not take account of all the facts, you can ask them to reconsider your application.</p>
+
+            <p>Blue badges allow people to benefit from certain parking concessions. These can vary between local authorities, though frequently unlimited parking is allowed in council car parks if you display a valid Blue Badge. Some local authorities require additional proof of a disability before allowing free parking - always check.</p>
+
+            <p>In addition, people who have a blue badge are often able to automatically access other concessions. For example, Blue Badge holders are able to gain exemption from the London Congestion Charge by registering with Transport for London in advance. There are also special car benefit rule adjustments for Blue Badge holders in employment which could mean they pay less tax.</p>
+
+            <p>There is a different way to apply in Northern Ireland:</p>
+
+            <p>https://www.nidirect.gov.uk/articles/rights-and-responsibilities-blue-badge-holders</p>
         </>
     );
 }
