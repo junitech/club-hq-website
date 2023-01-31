867ab3ec4710b41df0974c39edccc3e68b349530
Stuart Davies <stuart.davies@junitech.com>
2023-01-31 13:12:08 +0000

    Added all form fields

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -7,9 +7,27 @@ function ContactUs() {
             <div className="container-lg">
             <h1>Contact us</h1>
 
+            <p>If you are an existing HQA and would like to join Club HQ...</p>
+
+            <p>If you would like a mentor...</p>
+
+            <p>If you have any queries...</p>
+
+            <p>Please list your contact details below with your message and we will respond as soon as possible:</p>
+
             <form action="https://formspree.io/f/xayzqyna" method="POST">
-                <input type="email" name="_replyto" />
-                <textarea name="body"></textarea>
+                <label for="full-name">Full name:</label>
+                <input type="text" id="full-name" name="full-name" />
+
+                <label for="email-address">Email address:</label>
+                <input type="text" id="email-address" name="email-address" />
+
+                <label for="phone">Phone number:</label>
+                <input type="text" id="phone" name="phone" />
+
+                <label for="message">Message:</label>
+                <textarea id="message" name="message"></textarea>
+
                 <input type="submit" value="Send" />
             </form>
             </div>            
