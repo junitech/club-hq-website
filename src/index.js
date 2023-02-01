edc8552e44b6d749164a6be64656624cae0d89c4
Stuart Davies <stuart.davies@junitech.com>
2023-02-01 12:04:22 +0000

    Routing fixes

--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
 import NavBar from './Components/NavBar';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/club-hq-website">
       <NavBar />
 
       <div className='container-lg'>
