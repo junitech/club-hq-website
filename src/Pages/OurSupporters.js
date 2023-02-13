11d2143e74506032311c76959d8bfc07652d15cb
Stuart Davies <stuart.davies@junitech.com>
2023-02-13 16:53:21 +0000

    Refinement of links

--- src/Pages/OurSupporters.js
+++ src/Pages/OurSupporters.js
@@ -12,16 +12,15 @@ function OurSupporters() {
             <p><strong>The Access Card</strong> is a photo ID card for disabled people that communicates their access requirements (and legal rights) through a series of symbols on the card. The idea being that it is quick, easy and discreet for disabled people to ask for, and service providers to offer, critical reasonable adjustments.</p>
             <img src="/images/access-card.jpg" width={300} alt="The Access Card"></img>
 
-            <p>Visit the <a href="https://www.accesscard.online/find-a-credable-provider/map-of-providers/">website</a> to check out 1,000 venues and visitor attractions across the UK and beyond that recognise the Card</p>
+            <p>Visit the <a href="https://www.accesscard.online/find-a-credable-provider/map-of-providers/">Access Card</a> website to check out 1,000 venues and visitor attractions across the UK and beyond that recognise the Card</p>
 
             <h2>The Limbless Association</h2>
 
-            <p>The Limbless Association (LA) is a national charity that's been supporting the limb loss community for 37 years. The LA supports amputees pre and post-amputation with a range of services, resources and connectivity. These include the LA’s Help Desk (welfare rights, rehabilitation pathways, access to legal support and multi-agency signposting), LA Volunteer Visitor (peer support), Support and Connect Hubs (national), strategic representation on a number of national bodies, LA quarterly magazine StepForward and much more. LA membership is free to all amputees across the UK. Contact by phone 0800 644 0185 or <a href="mailto:enquires@limbless-association.org">enquires@limbless-association.org</a></p>
+            <p>The <a href="http://www.limbless-association.org">Limbless Association</a> (LA) is a national charity that's been supporting the limb loss community for 37 years. The LA supports amputees pre and post-amputation with a range of services, resources and connectivity. These include the LA’s Help Desk (welfare rights, rehabilitation pathways, access to legal support and multi-agency signposting), LA Volunteer Visitor (peer support), Support and Connect Hubs (national), strategic representation on a number of national bodies, LA quarterly magazine StepForward and much more. LA membership is free to all amputees across the UK. Contact by phone 0800 644 0185 or email to <a href="mailto:enquires@limbless-association.org">enquires@limbless-association.org</a></p>
 
             <p>Unit 10, Waterhouse Business Centre, 2 Cromar Way, Chelmsford, Essex, CM1 2QE</p>
             <p>01245 216670</p>
-            <p>www.limbless-association.org</p>
-            <p>http://www.facebook.com/limblessassociationhttp://www.twitter.com/limblessassoc</p>
+            <p><a href="http://www.limbless-association.org">Website</a> | <a href="http://www.facebook.com/limblessassociation">Facebook</a> | <a href="http://www.twitter.com/limblessassoc">Twitter</a></p>
 
             <img src="/images/limbless-association-banner.jpg" alt="Limbless Association"></img>
 
@@ -33,15 +32,13 @@ function OurSupporters() {
 
             <h2>Macmillan Cancer Support</h2>
 
-            <p>Macmillan offers comprehensive expert information and practical support on a wide range of matters.  From clinical and well-being information to legal and financial support and advice.  In some areas Macmillan volunteers offer practical support to patients on their return from hospital.</p>
-            <p>Find out more about how Macmillan can help YOU at https://www.macmillan.org.uk/cancer-information-and-support</p>
+            <p><a href="https://www.macmillan.org.uk/cancer-information-and-support">Macmillan Cancer Support</a> offers comprehensive expert information and practical support on a wide range of matters.  From clinical and well-being information to legal and financial support and advice.  In some areas Macmillan volunteers offer practical support to patients on their return from hospital.</p>
 
             <img src="/images/macmillan-cancer-support-logo.png" width={400} alt="Macmillan Cancer Support"></img>
 
             <h2>Sarcoma UK</h2>
 
-            <p>Sarcoma UK is a national charity that funds vital research, offers support for anyone affected by sarcoma and campaigns for better treatments. It is the only cancer charity in the UK focusing on all types of sarcoma.</p>
-            <p>https://sarcoma.org.uk/about-us</p>
+            <p><a href="https://sarcoma.org.uk/about-us">Sarcoma UK</a> is a national charity that funds vital research, offers support for anyone affected by sarcoma and campaigns for better treatments. It is the only cancer charity in the UK focusing on all types of sarcoma.</p>
   
             <p>Contact details:
  
