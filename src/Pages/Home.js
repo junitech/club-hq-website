c2eab242ae1244fbd016995d19e9046617356459
Stuart Davies <stuart.davies@junitech.com>
2023-02-16 12:19:29 +0000

    Fixed styling

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,12 +2,10 @@
 function Home() {
     return (
         <>
-            <figure className="text-center quote">
-                <blockquote>The odds of getting primary bone cancer and having to have a hindquarter amputation are similar to the odds of winning the Euromillions lottery</blockquote>
-                <figcaption>
+            <div className="text-center quote">
+                <blockquote>"The odds of getting primary bone cancer and having to have a hindquarter amputation are similar to the odds of winning the Euromillions lottery"</blockquote>
                     &mdash; <cite>Professor of Oncology, 2011</cite>
-                </figcaption>
-            </figure>
+            </div>
 
             <p>Welcome to the website of Club HQ, a mutual support service set up by hindquarter amputees for those with existing or upcoming hindquarter amputations. We are a UK organization but we have members from Eire, the United States and New Zealand.</p>
         </>
