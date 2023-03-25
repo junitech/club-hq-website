a633625bc169465941fac918c6db56ed23c27b3a
Claire Davies <claire.davies@junitech.com>
2023-03-25 01:53:36 +0000

    Added cards and background colours

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+    return (
+
+        <footer className="footer"><span className="copyright">© 2021 All Rights Reserved</span></footer>
+
+    );
+}
+
+export default Footer;
