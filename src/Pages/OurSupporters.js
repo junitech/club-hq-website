3669fb11231467b802c1313a6c79279cfe7e9f4f
Claire Davies <claire.davies@junitech.com>
2023-05-02 23:18:57 +0100

    add border to container

--- src/Pages/OurSupporters.js
+++ src/Pages/OurSupporters.js
@@ -2,33 +2,9 @@
 function OurSupporters() {
     return (
         <>
-            <div class="container">
+          
             <h1>Our supporters</h1>
-            {/* <h2>Sarcoma UK</h2>
-
-            <div className="card" >
-                <div className="row g-0">
-                    <div className="col-md-4">
-                        <img src="/images/sarcoma-uk-logo.jpg" class="card-img-top" alt="Sarcoma UK"></img>
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                           
-                            <p className="card-text"><a href="https://sarcoma.org.uk/about-us" target="_blank" rel="noreferrer">Sarcoma UK</a> is a national charity that funds vital research, offers support for anyone affected by sarcoma and campaigns for better treatments. It is the only cancer charity in the UK focusing on all types of sarcoma.</p>
-                            <p>Please contact the Sarcoma UK support line with any questions, or just to talk.</p>
-
-                            <p>Contact details:
- 
-                            Sarcoma UK Support Line:
-                            Freephone: 0808 801 401 (open Monday -Friday, 10am - 3pm)
-                            Email: supportline@sarcoma.org.uk
-                            Text: 07860 058 830 
-                            </p>
-
-                        </div>
-                    </div>
-                </div>
-             </div>  */}
+           
             <section>
             <h2>Sarcoma UK</h2>
 
@@ -46,51 +22,9 @@ function OurSupporters() {
             </p>
             </section>
 
-            {/* <h2>Nimbus and The Access Card</h2>
-            <div className="card mb-3">
-                <div className="row g-0">
-                    <div className="col-md-4">
-                        <img src="/images/nimbus-disability-logo.jpg" class="img-fluid rounded-start" alt="Nimbus Disability logo"></img>
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                           
-                            <p className="card-text">Nimbus Disability is a social Enterprise set up to support businesses to meet and exceed their legal obligations toward disabled people. An outcome of our 15+ years of experience became a scheme called the <a href="https://www.accesscard.online/" target="_blank" rel="noreferrer">Access Card</a>.</p>
-                            
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="card mb-3">
-                <div className="row g-0">
-                    <div className="col-md-4">
-                        <img src="/images/access-card.jpg" class="img-fluid rounded-start" alt="The Access Card"></img>
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                           
-                            <p className="card-text"><strong>The Access Card</strong> is a photo ID card for disabled people that communicates their access requirements (and legal rights) through a series of symbols on the card. The idea being that it is quick, easy and discreet for disabled people to ask for, and service providers to offer, critical reasonable adjustments.</p>
-                            <p>Visit the <a href="https://www.accesscard.online/find-a-credable-provider/map-of-providers/" target="_blank" rel="noreferrer">Access Card</a> website to check out 1,000 venues and visitor attractions across the UK and beyond that recognise the Card</p>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="card">
-                <div className="col-sm-4">
-                <img src="/images/queens-award-for-enterprise-logo.png" width={100} alt="Queen's Award for Enterprise" class="align-right"></img>
-
-                </div>
-            </div> */}
+           
             <section>
                 <h2>Nimbus and The Access Card</h2>
-
-
-            {/* <img src="/images/nimbus-disability-logo.jpg" alt="Nimbus Disability logo" class="align-right"></img>
-            <img src="/images/queens-award-for-enterprise-logo.png" width={100} alt="Queen's Award for Enterprise" class="align-right"></img>
-            <img src="/images/access-card.jpg" width={300} alt="The Access Card" class="align-right"></img> */}
            <img src= "/images/nimbus-access-collage.png" class= "center-image" alt = "Access card and nimbus disability logo"></img>
 
             <p>Nimbus Disability is a social Enterprise set up to support businesses to meet and exceed their legal obligations toward disabled people. An outcome of our 15+ years of experience became a scheme called the <a href="https://www.accesscard.online/" target="_blank" rel="noreferrer">Access Card</a>.</p>
@@ -100,25 +34,6 @@ function OurSupporters() {
             <p>Visit the <a href="https://www.accesscard.online/find-a-credable-provider/map-of-providers/" target="_blank" rel="noreferrer">Access Card</a> website to check out 1,000 venues and visitor attractions across the UK and beyond that recognise the Card</p>
             </section>
 
-            {/* <h2>The Limbless Association</h2>
-
-            <div className="card mb-3">
-                
-                    <div className="col-md-8">
-                        <img src="/images/limbless-association-banner.jpg" class="card-img-top" alt="Limbless Association"></img>
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                           
-                            <p className="card-text">The <a href="http://www.limbless-association.org" target="_blank" rel="noreferrer">Limbless Association</a> (LA) is a national charity that's been supporting the limb loss community for 37 years. The LA supports amputees pre and post-amputation with a range of services, resources and connectivity. These include the LA’s Help Desk (welfare rights, rehabilitation pathways, access to legal support and multi-agency signposting), LA Volunteer Visitor (peer support), Support and Connect Hubs (national), strategic representation on a number of national bodies, LA quarterly magazine StepForward and much more. LA membership is free to all amputees across the UK. Contact by phone 0800 644 0185 or email to <a href="mailto:enquires@limbless-association.org">enquires@limbless-association.org</a></p>
-                            <p>Unit 10, Waterhouse Business Centre, 2 Cromar Way, Chelmsford, Essex, CM1 2QE</p>
-                            <p>01245 216670</p>
-                            <p><a href="http://www.limbless-association.org" target="_blank" rel="noreferrer">Website</a> | <a href="http://www.facebook.com/limblessassociation" target="_blank" rel="noreferrer">Facebook</a> | <a href="http://www.twitter.com/limblessassoc" target="_blank" rel="noreferrer">Twitter</a></p>
-
-                        </div>
-                    
-                </div>
-            </div> */}
             <section>
             <h2>The Limbless Association</h2>
             
@@ -132,23 +47,7 @@ function OurSupporters() {
             <p><a href="http://www.limbless-association.org" target="_blank" rel="noreferrer">Website</a> | <a href="http://www.facebook.com/limblessassociation" target="_blank" rel="noreferrer">Facebook</a> | <a href="http://www.twitter.com/limblessassoc" target="_blank" rel="noreferrer">Twitter</a></p>
             </section>
 
-            {/* <h2>The Bone Cancer Research Trust</h2>
-
-            <div className="card mb-3">
-               
-                    <div className="col-md-8">
-                        <img src="/images/bone-cancer-research-trust-banner.jpg" class="card-img-top" alt="Bone Cancer Research Trust"></img>
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                           
-                            <p className="card-text"><p>The Bone Cancer Research Trust is the leading charity dedicated to ﬁghting primary bone cancer and bone tumours. Our mission is to save lives and improve outcomes for people affected by primary bone cancer through research, information, awareness and support.</p></p>
-                            <p>The Bone Cancer Research Trust funds pioneering research into all forms of primary bone cancer and ensures that accurate information and support are available for patients of all ages whilst raising national and international awareness of this devastating disease. To find out more about the charity please visit bcrt.org.uk or for any help or support contact our Support & Information Service on 0800 111 4855/support@bcrt.org.uk</p>
-
-                        </div>
-                    </div>
-               
-            </div> */}
+            
             <section>
             <h2>The Bone Cancer Research Trust</h2> 
             <img src="/images/bone-cancer-research-trust-banner.jpg" class="center-image" alt="Bone Cancer Research Trust"></img>
@@ -162,49 +61,11 @@ function OurSupporters() {
                 <p><a href="https://www.macmillan.org.uk/cancer-information-and-support" target="_blank" rel="noreferrer">Macmillan Cancer Support</a> offers comprehensive expert information and practical support on a wide range of matters.  From clinical and well-being information to legal and financial support and advice.  In some areas Macmillan volunteers offer practical support to patients on their return from hospital.</p>
             </section>
 
-
-            {/* <h2>Macmillan Cancer Support</h2>
-
-            <div className="card mb-3">
-                <div className="row g-0">
-                    <div className="col-md-4">
-                        <img src="/images/macmillan-cancer-support-logo.png" class="img-fluid rounded-start" alt="Macmillan Cancer Support"></img>
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                            <p className="card-text"><a href="https://www.macmillan.org.uk/cancer-information-and-support" target="_blank" rel="noreferrer">Macmillan Cancer Support</a> offers comprehensive expert information and practical support on a wide range of matters.  From clinical and well-being information to legal and financial support and advice.  In some areas Macmillan volunteers offer practical support to patients on their return from hospital.</p>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
             <section>
             <h2>Sidestix</h2>
             <img src="/images/sidestix-logo.png" class="center-image" alt="Sidestix"></img>
             <p>See the Secret Facebook Group for Club HQ discount code.</p>
             </section>
-
-            {/* <h2>Sidestix</h2>
-            <div className="card mb-3">
-                <div className="row g-0">
-                    <div className="col-md-4">
-                        <img src="/images/sidestix-logo.png" width={300} class="align-left" alt="Sidestix"></img>
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                            <p className="card-text">See the Secret Facebook Group for Club HQ discount code.</p>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
-
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            </div>
 
         </>
     );
