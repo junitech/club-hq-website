16646368f0775cd4a27afd5dc5dc2cd3c9cbae65
Stuart Davies <stuart.davies@junitech.com>
2023-05-09 17:41:41 +0100

    Updates to Phanton Pain

--- src/Pages/PhantomPain.js
+++ src/Pages/PhantomPain.js
@@ -4,10 +4,11 @@ function PhantomPain() {
         <>
             <h1>Phantom pain</h1>
 
-            <p>Author: Dr Dominic Aldington FRCA FFPMRCA</p>
-            <p>has specialised exclusively in pain management for the last 10 years and lectured on the subject around the world. He is the Clinical Lead and Consultant in Pain Medicine at Winchester Hospital and Hampshire Hospitals NHS Trust. Having previously served in the Royal Army Medical Corps for 20 years, his many appointments include Chairman of the UK Armed Forces Clinical Forum, Vice-chairman of the IASP Special Interest Group on the Pain of Torture, Organised Violence and War, and the Surgeon General's Advisor of Veterans' Health and is involved in the development of pain clinics for military veterans.</p>
+            <p>Dr Dominic Aldington FRCA FFPMRCA has specialised exclusively in pain management for the last 10 years and lectured on the subject around the world. He is a Consultant in Pain Medicine at Winchester Hospital and Hampshire Hospitals NHS Trust. Having previously served in the Royal Army Medical Corps for over 20 years, his many appointments include Chairman IASP Special Interest Group on Pain and Trauma, and is involved in the development of pain clinics for military veterans.</p>
 
-            <p>I've been invited to write a few words on the subject of pain for your group. However, before I do I would like to start by saying what a great project I think this is and how honored I am to have been invited to participate.</p>
+            <hr/>
+
+            <p>I've been invited to write a few words about pain for your group. However, before I do I would like to start by saying what a great project I think this is and how honoured I am to have been invited to participate.</p>
 
             <p>I won't bore you with my credentials as you are quite capable of hunting me down on the Internet.</p>
 
@@ -30,23 +31,23 @@ function PhantomPain() {
 
             <p>This highlights the importance of both the emotional and sensory components of pain, the personal nature of pain and that further damage does not have to be present for the pain to be experienced. Indeed, in the absence of damage, pain is still pain.</p>
 
-            <p>One of the many problems with pain is that we are led to believe that it always indicates damage and that the damage and the pain perceived are in some way proportional to one another. Thus, if you have big pain you must have big damage and vice a versa. Unfortunately, this is a fallacy and you can have dreadful pain and little damage, and you can have dreadful damage and little pain. In either way, if we are treating pain, then pain is real whenever anyone says it is.</p>
+            <p>One of the many problems with pain is that we are led to believe that it always indicates damage and that the damage and the pain perceived are in some way proportional to one another. Thus, if you have big pain, you must have big damage and vice a versa. Unfortunately, this is a fallacy, and you can have dreadful pain and little damage, and you can have dreadful damage and little pain. In either way, if we are treating pain, then pain is real whenever anyone says it exists.</p>
 
             <p>The other point is that we, healthcare workers, cannot tell you how bad your pain is any more than you can tell us how bad our pain is. Pain is a personal thing and will vary. The good news is I do not need to tell you how bad your pain is, as you can tell me.</p>
 
-            <p>The mechanisms for phantom sensation and phantom pain are not agreed and there are a number of theories. When discussing pains associated with amputation healthcare workers often get very excited and erroneously ascribe everything to phantom pain. Phantom pain describes the pain in the part of the body that no longer exists. Phantom sensation describes a non-painful sensation in the part of the body that no longer exists. A stump pain is a pain in the "residual limb" if there is any.</p>
+            <p>The mechanisms for phantom sensation and phantom pain are not agreed and there are a number of theories. When discussing pains associated with amputation healthcare workers often get very excited and erroneously ascribe everything to phantom pain. Phantom pain describes the pain in the part of the body that no longer exists. Phantom sensation describes a non-painful sensation in the part of the body that no longer exists. A stump pain is a pain in the "residual limb", if there is any.</p>
 
-            <p>Similarly, the best treatment modalities are also unclear, and it seems likely that an individual needs to work through quite a list of things until they find something that works for them. No intervention is likely to remove all the pain and in fact the aim should be providing a treatment that stops the pain being a problem. Research would suggest people can cope with a mild pain but when the pain gets more severe than mild, coping mechanisms fail.</p>
+            <p>Similarly, the best treatment modalities are also unclear, and it seems likely that an individual needs to work through quite a list of things until they find something that works for them. No intervention is likely to remove all the pain and in fact the aim should be providing a treatment that stops the pain being a problem. Research would suggest people can cope with a “mild” pain but when the pain gets more severe than mild, coping mechanisms fail.</p>
 
             <p>So now we understand that pain is a fairly complex thing we need to discuss the options for its management.</p>
 
-            <p>I would suggest the first thing to do with the pain it's understand what it means to you. If you believe it indicates something terrible going on, then you will need to have it investigated and treated invasively. Sadly, this may not change the pain and can even make it worse. If you accept the pain is there, it is real, but it does not indicate anything dreadful happening, then you are more likely to be able to engage in symptom management and master your pain.</p>
+            <p>I would suggest the first thing to do with the pain is to  understand what it means to you. If you believe it indicates something terrible going on, then you will need to have it investigated and treated invasively. Sadly, this may not change the pain and can even make it worse. If you accept the pain is there, it is real, but it does not indicate anything dreadful happening, then you are more likely to be able to engage in symptom management and master your pain.</p>
 
-            <p>It is probably fair to say that the longer one has a pain the more significant the emotional component becomes. Broadly speaking, the emotional component is made up of anxiety, unhappiness, and frustration (known by many of my patients as the “F' in pain”).</p>
+            <p>It is probably fair to say that the longer one has a pain the more significant the emotional component becomes. Broadly speaking, the emotional component is made up of anxiety, unhappiness, and frustration (known by many of my patients as the “F' in pain”, and representing the difference between where we expect to be and where we are.</p>
 
             <p>For the emotional components, one can look at specific psychological therapies or pain management programs, and some forms of medication may be of benefit.</p>
 
-            <p>For the physical components of pain there are a number of creams and gels, and pills and potions that can be taken but none of these are wonderful for everyone and very often for some people they cause more trouble than they're worth.</p>
+            <p>For the physical components of pain there are a number of creams and gels, and pills and potions that can be taken but none of these are wonderful for everyone and very often they cause more trouble than they're worth.</p>
 
             <p>Another concept to manage is the ownership of the pain. Many healthcare professionals will offer to help and in doing so may subjugate your position. However, the pain is yours and you must make the decisions about what you do, since only you will get benefits and only you will suffer side-effects and it is your life that is being affected.</p>
 
